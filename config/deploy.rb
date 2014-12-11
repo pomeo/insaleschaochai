26ac2d16c620ec082805018515c280732f962ca2
Sergey Ovechkin <me@sovechkin.com>
2014-12-11 21:42:58 +0300

    capistrano, переезд с сервера на DO

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,12 +8,19 @@ set :application, "test1.sovechkin.com"
 require           "capistrano-offroad"
 offroad_modules   "defaults", "supervisord"
 set :repository,  "git@github.com:pomeo/insaleschaochai.git"
-set :supervisord_start_group, "test"
-set :supervisord_stop_group, "test"
+set :deploy_to,   "/home/ubuntu/www/chaochai"
+set :supervisord_start_group, "chaochai"
+set :supervisord_stop_group, "chaochai"
 #========================
 #ROLES
 #========================
-set  :gateway,    "#{application}"   # main server
-role :app,        "ubuntu@10.3.42.1" # lxc container
+role :app,        "ubuntu@#{application}"
 
-after "deploy:create_symlink", "deploy:npm_install", "deploy:restart"
+namespace :deploy do
+  desc "Change node.js port"
+  task :chg_port do
+    run "sed -i 's/3000/7000/g' #{current_path}/app.js"
+  end
+end
+
+after "deploy:create_symlink", "deploy:npm_install", "deploy:chg_port", "deploy:restart"
